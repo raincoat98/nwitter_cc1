ceed3de96ccfa85d69f98162ee6a45412b011a2a
ww57403 <ww57403@naver.com>
2021-10-05 16:17:40 +0900

    Edit : router 에러 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router.dom";
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <Router>
       <Switch>
         {isLoggedIn ? (
-          <>
+          <React.Fragment>
             <Route exact path="/">
               <Home></Home>
             </Route>
-          </>
+          </React.Fragment>
         ) : (
-          <>
+          <React.Fragment>
             <Route exact path="/">
               <Auth></Auth>
             </Route>
-          </>
+          </React.Fragment>
         )}
       </Switch>
     </Router>
