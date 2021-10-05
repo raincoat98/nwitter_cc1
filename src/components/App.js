c3260cae27ab84aecf855e3f40be864ed3386913
ww57403 <ww57403@naver.com>
2021-10-05 16:46:34 +0900

    Using Firebase Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./Router";
+import { auth } from "firebase";
 
 function App() {
-  return <AppRouter></AppRouter>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <React.Fragment>
+      <AppRouter isLoggedIn={isLoggedIn}></AppRouter>
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </React.Fragment>
+  );
 }
 
 export default App;
