33a42e14356bd7b533f1e4792236ed17cd4176b8
ww57403 <ww57403@naver.com>
2021-10-06 15:26:03 +0900

    Add: Logout Process

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "components/Navigation";
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <React.Fragment>
             <Route exact path="/">
               <Home></Home>
+            </Route>
+            <Route exact path="/profile">
+              <Profile></Profile>
             </Route>
           </React.Fragment>
         ) : (
@@ -17,6 +28,7 @@ const AppRouter = ({ isLoggedIn }) => {
             <Route exact path="/">
               <Auth></Auth>
             </Route>
+            <Redirect from="*" to="/" />
           </React.Fragment>
         )}
       </Switch>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,17 @@
+import { authService } from "fBase";
 import React from "react";
+import { useHistory } from "react-router";
 
-export default () => <span>EditProfile</span>;
+export default () => {
+  const history = useHistory();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <React.Fragment>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </React.Fragment>
+  );
+};
